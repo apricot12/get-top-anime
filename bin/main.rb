df24d332e13be865b93bd5624770b4cd39742a97
Nihal Mohammed <mnihal64@outlook.com>
2020-12-23 00:48:53 +0530

    implement json export

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 require './lib/scraper'
 
 scraper = AnimeList.new
 scraper.scraper
+scraper.format_json
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,49 +2,46 @@
 
 require 'nokogiri'
 require 'httparty'
-require 'csv'
+require 'json'
 
+#:nodoc:
 class AnimeList
-
   def scraper
-    url = 'https://myanimelist.net/topanime.php?'
-    unpar_pg = HTTParty.get(url)
-    par_pg = Nokogiri::HTML(unpar_pg)
     @animes = []
-    anime_list = par_pg.css('tr.ranking-list')
     page = 0
     last_page = 950
     while page <= last_page
       pagin_url = "https://myanimelist.net/topanime.php?limit=#{page}"
-      puts "#{page}"
-      puts ''
       pagin_unpar_pg = HTTParty.get(pagin_url)
-      pagin_par_pg = Nokogiri::HTML(pagin_unpar_pg)
+      pagin_par_pg = Nokogiri::HTML(pagin_unpar_pg.body)
       @pagin_anime_list = pagin_par_pg.css('tr.ranking-list')
       pagination_anime_list
       page += 50
     end
   end
 
-  def export_to_csv
-      CSV.open("myfile.csv", "w") do |csv|
-          csv << @animes
-      end
+  def export_to_json
+    File.open('./json/anime_list.json', 'w') do |f|
+      f.write(@animes.to_json)
+    end
   end
-    
+
+  def format_json
+    json = JSON.parse File.read './json/anime_list.json'
+    File.open('./json/anime_list.json', 'w') do |list|
+      list.write './json/anime_list.json', JSON.pretty_generate(json)
+    end
+  end
+
   def pagination_anime_list
     @pagin_anime_list.each do |anime_listing|
-      anime =  {
+      anime = {
         rank: anime_listing.css('span.lightLink').text,
         name: anime_listing.css('h3.hoverinfo_trigger').text,
         rating: anime_listing.css('span.text.on.score-label').text
-      } 
+      }
       @animes << anime
-      export_to_csv
-      puts "Added #{anime[:name]}"
-      puts ''
+      export_to_json
     end
   end
 end
-
-
