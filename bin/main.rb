79509d1307db88e467a42b70c15b1825f3d925c5
Nihal Mohammed <mnihal64@outlook.com>
2020-12-23 13:04:24 +0530

    add spec and microverse linter version

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require './lib/scraper'
 
 scraper = AnimeList.new
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,7 @@
-# frozen_string_literal: true
-
 require 'nokogiri'
 require 'httparty'
 require 'json'
 
-#:nodoc:
 class AnimeList
   def scraper
     @animes = []
